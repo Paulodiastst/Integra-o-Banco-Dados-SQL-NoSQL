53c864279713aaf108659766d42af5978aac5d5f
Paulo Roberto <79487813+Paulodiastst@users.noreply.github.com>
2022-06-04 13:24:47 -0300

    Hands on - Programando a Classe Aluno na Camada Model do Projeto

--- app/src/main/java/app/fd/db/v1/model/Aluno.java
+++ app/src/main/java/app/fd/db/v1/model/Aluno.java
@@ -1,4 +1,50 @@
 package app.fd.db.v1.model;
 
 public class Aluno {
+
+    private int id;
+    private String nome;
+    private String email;
+    private boolean status;
+
+    public Aluno() {}
+
+    public Aluno(int id, String nome, String email, boolean status) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
